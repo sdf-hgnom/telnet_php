a538c4cec001f878aab062b3fa4e413144d1ade2
sdf-hgnom <shulinab@yandex.ru>
2021-08-28 10:36:44 +0800

    add media

--- index_1.php
+++ index_1.php
@@ -9,14 +9,24 @@ include "blocks/headers_no_map.php";
 require "config.php";
 global $all_cameras;
 ?>
+<!-- Кнопка акардеона   &#9776;-->
     <body>
-    <header class="main_header">
-        <a href="#"> <img src="images/telnet_logo.png" alt="logo_telnet"> </a>
-        <h1 class="page_header"> Камеры Усть-Илимска </h1>
-        <a href="source/signout.php"> Выйти  </a>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#" > <img class="foto" src="images/telnet_logo.png" alt="foto"></a>
+            <h1 class="page_header"> Камеры Усть-Илимска </h1>
+            <a href="source/signout.php"> Выйти  </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="aside" aria-controls="aside" aria-expanded="true" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
 
-    </header>
-    <div class="container">
+
+        </div>
+    </nav>
+    <!--    <header class="main_header">-->
+<!---->
+<!--    </header>-->
+    <div class="container-fluid">
         <div class="context">
             <div id="map" class="my_map">
             </div>
